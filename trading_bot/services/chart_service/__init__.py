4982a1589e27f27833ede057322d3dc7a751700e
SigmaPips <jovanni@sigmapips.com>
2025-05-03 10:02:50 +0700

    Fix: Remove DirectYahooProvider references that were not in original codebase

--- trading_bot/services/chart_service/__init__.py
+++ trading_bot/services/chart_service/__init__.py
@@ -3,7 +3,6 @@ Chart Service module
 """
 import logging
 from trading_bot.services.chart_service.chart import ChartService as _ChartService
-from trading_bot.services.chart_service.direct_yahoo_provider import DirectYahooProvider
 from trading_bot.services.chart_service.yfinance_provider import YahooFinanceProvider
 from trading_bot.services.chart_service.binance_provider import BinanceProvider
 
